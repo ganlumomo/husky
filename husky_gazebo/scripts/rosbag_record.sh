467749cbafff14f9c982926fa324fcfd5c3edc08
Lu Gan <ganlu@umich.edu>
2023-07-30 01:22:07 -0700

    added ekf odom in bag recording

--- husky_gazebo/scripts/rosbag_record.sh
+++ husky_gazebo/scripts/rosbag_record.sh
@@ -1,1 +1,1 @@
-rosbag record /husky_velocity_controller/cmd_vel /orb_slam2_rgbd/pose /elevation_mapping/elevation_map_raw /tf /tf_static /gazebo/model_states
+rosbag record /husky_velocity_controller/cmd_vel /orb_slam2_rgbd/pose /odometry/filtered /elevation_mapping/elevation_map_raw /tf /tf_static /gazebo/model_states
